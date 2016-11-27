dd86537cf3a7c2062b6a6cf9f9eb87865a525460
Matt Distefano <eponymous@mattdistefano.com>
2016-11-27 11:01:41 -0800

    Initial commit of WIP

---
+++ webpack.config.js
@@ -0,0 +1,159 @@
+// borrowed in part from https://github.com/preboot/angular2-webpack
+
+// Helper: root() is defined at the bottom
+var path = require('path');
+var webpack = require('webpack');
+
+// Webpack Plugins
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+/**
+ * Env
+ * Get npm lifecycle event to identify the environment
+ */
+var ENV = process.env.npm_lifecycle_event;
+
+var isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig() {
+  /**
+   * Config
+   * Reference: http://webpack.github.io/docs/configuration.html
+   * This is the object where all configuration gets set
+   */
+  var config = {};
+
+  /**
+   * Devtool
+   * Reference: http://webpack.github.io/docs/configuration.html#devtool
+   * Type of sourcemap to use per build type
+   */
+  if (isProd) {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval-source-map';
+  }
+
+  /**
+   * Entry
+   * Reference: http://webpack.github.io/docs/configuration.html#entry
+   */
+  config.entry = {
+    'main': './src/js/main.js'
+  };
+
+  /**
+   * Output
+   * Reference: http://webpack.github.io/docs/configuration.html#output
+   */
+  config.output = {
+    path: root('dist'),
+    publicPath: '',
+    filename: isProd ? 'js/[name].[hash].js' : 'js/[name].js'
+  };
+
+  /**
+   * Resolve
+   * Reference: http://webpack.github.io/docs/configuration.html#resolve
+   */
+  config.resolve = {
+    // only discover files that have those extensions
+    extensions: ['.js', '.scss', '.html'],
+  };
+
+  /**
+   * Loaders
+   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+   * List: http://webpack.github.io/docs/list-of-loaders.html
+   * This handles most of the magic responsible for converting modules
+   */
+  config.module = {
+    rules: [{
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'url-loader?name=assets/[name].[hash].[ext]&limit=10000',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              mozjpeg: {
+                quality: 65
+              },
+            }
+          }
+        ]
+      },
+      // support for .scss files
+      // all scss will be bundled in an external css file
+      {
+        test: /\.(scss|sass)$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+      },
+
+      // support for .html
+      // todo: change the loader to something that adds a hash to images
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        query: {
+          minimize: isProd
+        }
+      }
+    ]
+  };
+
+  /**
+   * Plugins
+   * Reference: http://webpack.github.io/docs/configuration.html#plugins
+   * List: http://webpack.github.io/docs/list-of-plugins.html
+   */
+  config.plugins = [
+    // Inject script and link tags into html files
+    // Reference: https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunksSortMode: 'dependency'
+    }),
+
+    // Extract css files
+    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+    // Disabled when in test mode or not in build mode
+    new ExtractTextPlugin({ filename: 'css/[name].[hash].css', disable: !isProd })
+  ];
+
+  // Add build specific plugins
+  if (isProd) {
+    config.plugins.push(
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Only emit files when there are no errors
+      new webpack.NoErrorsPlugin()
+    );
+  }
+
+  /**
+   * Dev server configuration
+   * Reference: http://webpack.github.io/docs/configuration.html#devserver
+   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+   */
+  config.devServer = {
+    contentBase: './src/',
+    historyApiFallback: true,
+    quiet: true,
+    stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose,
+  };
+
+  return config;
+}();
+
+// Helper functions
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
