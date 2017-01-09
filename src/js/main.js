6095ed73310378f46aaac2447ee9d518a027e1b9
Matt Distefano <eponymous@mattdistefano.com>
2017-01-08 16:35:15 -0800

    Reset scroll position

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
 import '../scss/main.scss';
+
+// reset scroll position on unload so it's not remembered when refreshing
+window.addEventListener('unload', function(e){
+   window.scrollTo(0,0);
+});
